a7e6d0e7a9901a136ac64bcd956fa3ae299c2cfc
Eddy Tyannikov <etyannikov@gmail.com>
2019-11-15 18:28:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,7 @@ class Student
     SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
     DB[:conn].execute(sql, name).map do |row|
-      self.find_by_name(row)
+      self.new_from_db(row)
     end.first
   end
   
